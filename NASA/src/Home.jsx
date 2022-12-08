3e4584b088c80f1ef206ccd724b618fc5f034373
Amneet Singh <+>
2022-12-08 17:21:13 -0500

    positions

--- NASA/src/Home.jsx
+++ NASA/src/Home.jsx
@@ -8,10 +8,10 @@ export default function Home({ planets }) {
             <br/>
             <br/>
             <div className="nav-container">
-                <button className='button' onClick={() => navigate('/yt')}>Videos Of Our Solar System</button>
-                <button className='button' onClick={() => navigate('/misson')}>Space Missions</button>
-                <button className='button' onClick={() => navigate('/spaceimg')}>Space Images</button>
-                <button className='button' onClick={() => navigate('/quiz')}>Space Quiz</button>
+                <button className='button' onClick={() => navigate('/yt')}>Videos Of Our Solar System</button><br/>
+                <button className='button' onClick={() => navigate('/misson')}>Space Missions</button><br />
+                <button className='button' onClick={() => navigate('/spaceimg')}>Space Images</button><br />
+                <button className='button' onClick={() => navigate('/quiz')}>Space Quiz</button><br />
             </div>
            
             <div className="main">
